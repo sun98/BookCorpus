67a374b1f9fdb8f3b1e52dc4b93629fe55ce6bfe
sun98 <1329575336@qq.com>
2018-11-12 21:49:38 +0800

    Initial commit

---
+++ image_crawler.py
@@ -0,0 +1,11 @@
+"""
+@Author: Sun Suibin 
+@Date: 2018-11-12 21:33:08 
+@Last Modified by:   Sun Suibin 
+@Last Modified time: 2018-11-12 21:33:08 
+"""
+
+from icrawler.builtin import GoogleImageCrawler
+
+google_crawler = GoogleImageCrawler(storage={'root_dir': 'images'})
+google_crawler.crawl(keyword='dodo', max_num=100, min_size=(200, 200))
