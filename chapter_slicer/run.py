acb863f7ae2bdb1ad2f7794bf17906cdc3caf3f0
wjw <2353330493@qq.com>
2018-11-16 19:49:33 +0800

    Update run.py

---
+++ chapter_slicer/run.py
@@ -0,0 +1,37 @@
+import json
+import multiprocessing
+import os
+import urllib.request
+import urllib
+import re
+from multiprocessing import Process
+from multiprocessing.pool import Pool
+from urllib import request
+
+def FindChapter(content):
+    pattern = re.compile('<h[12345]>\n?.+\n?</h[12345]>')
+    s = re.search(pattern, content)
+    if s == None:
+        return "None"
+    span = s.span()
+    return span
+
+if __name__ == "__main__":
+    f = open('tag.json')
+    data = json.load(f)
+    s = 0
+    t = 0
+    for i in data.keys():
+        name = 'raw_data/f'+ i + '.html'
+        print(s,t)
+        print(name, )
+        t += 1
+        try:
+            book = open(name,encoding='utf-8').read()
+        except:
+            book = open(name,encoding='ISO8859-1').read()
+
+        span = FindChapter(book)
+        if span == "None":
+            s += 1
+        print(span)
