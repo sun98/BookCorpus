c2b8dd09de784782c321d8b1f3df648cf648274e
sun98 <1329575336@qq.com>
2018-12-08 18:49:56 +0800

    cancel ignore of  db_config

---
+++ app/db_config.py
@@ -0,0 +1,11 @@
+"""
+@Author: Sun Suibin
+@Date: 2018-12-02 19:52:30
+@Last Modified by:   Sun Suibin
+@Last Modified time: 2018-12-02 19:52:30
+"""
+
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DB_PW = 'your_password'
+DB_NAME = {1: 'bookcorpus1', 2: 'bookcorpus2'}
